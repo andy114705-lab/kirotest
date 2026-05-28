9df91d336c287575cc2c21623409d3f12e5181f1
andy <andy114705@gmail.com>
2026-05-28 22:40:28 +0800

    Add files via upload

---
+++ ag-grid-en.js
@@ -0,0 +1,11 @@
+export const agGridEn = {
+  // Layout:
+  auto: 'Auto',
+  fixed: 'Fixed',
+  paging: 'Paging',
+  eachPage: 'Each Page',
+  lineHeight: 'Line Height',
+  header: 'Header',
+  row: 'Row',
+  restoreDefault: 'Restore Default',
+}
